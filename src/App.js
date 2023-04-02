e40350bc670958f3c7881e48bc8f32309d4d521a
andrieljose <andrielscript@gmail.com>
2023-04-02 11:46:25 -0300

    add feats to App.js, index.css, index,js and create useMediaQuery
    feat: add Navbar, DotGroup and Landing scenes with state management and media query hook in App component.
    
    add useMediaQuery hook and implement scroll event listener in App.js

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import Navbar from "./scenes/Navbar";
+import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
+import { useState, useEffect } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isDesktop && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
