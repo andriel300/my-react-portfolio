003e0886d97490c743b5c073d5537b25c6c66327
andrieljose <andrielscript@gmail.com>
2023-04-02 17:08:49 -0300

    feat: add Contact and Footer sections to App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Landing from "./scenes/Landing";
 import MySkills from "./scenes/MySkills";
 import Projects from "./scenes/Projects";
 import Testimonials from "./scenes/Testimonials";
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
 import { useState, useEffect } from "react";
 import LineGradient from "./components/LineGradient";
 import useMediaQuery from "./hooks/useMediaQuery";
@@ -50,6 +52,11 @@ function App() {
       <div className="w-5/6 mx-auto md:h-full">
         <Testimonials />
       </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
 }
