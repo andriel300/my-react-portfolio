fb8c21e9729e5580db2921a90bae22cecca1c7d5
andrieljose <andrielscript@gmail.com>
2023-04-04 11:43:03 -0300

    Create skills icons component

---
+++ src/components/SkillsIcons.jsx
@@ -0,0 +1,58 @@
+const SkillsIcons = () => {
+  return (
+    <div className="flex flex-wrap flex-row justify-center md:justify-start my-10 gap-7">
+      <img
+        className="w-10 md:w-10 hover:opacity-50 transition duration-500"
+        alt="JavaScript"
+        src="../assets/JavaScript.png"
+      />
+      <img
+        className="w-10 md:w-10 hover:opacity-50 transition duration-500"
+        alt="HTML5"
+        src="../assets/HTML5.png"
+      />
+      <img
+        className="w-10 md:w-10 hover:opacity-50 transition duration-500"
+        alt="CSS3"
+        src="../assets/CSS3.png"
+      />
+      <img
+        className="w-10 md:w-10 hover:opacity-50 transition duration-500"
+        alt="React"
+        src="../assets/React.png"
+      />
+      <img
+        className="w-10 md:w-10 hover:opacity-50 transition duration-500"
+        alt="Redux"
+        src="../assets/Redux.png"
+      />
+      <img
+        className="w-10 md:w-10 hover:opacity-50 transition duration-500"
+        alt="Node.js"
+        src="../assets/Node.js.png"
+      />
+      <img
+        className="w-10 md:w-10 hover:opacity-50 transition duration-500"
+        alt="MongoDB"
+        src="../assets/MongoDB.png"
+      />
+      <img
+        className="w-10 md:w-10 hover:opacity-50 transition duration-500"
+        alt="PostgresSQL"
+        src="../assets/PostgresSQL.png"
+      />
+      <img
+        className="w-10 md:w-10 hover:opacity-50 transition duration-500"
+        alt="tailwind-CSS"
+        src="../assets/Tailwind-CSS.png"
+      />
+      <img
+        className="w-10 md:w-10 hover:opacity-50 transition duration-500"
+        alt="Vite.js"
+        src="../assets/Vite.js.png"
+      />
+    </div>
+  );
+};
+
+export default SkillsIcons;
