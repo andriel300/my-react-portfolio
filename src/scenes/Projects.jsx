1aae7069e2a06b08e4458df70ec7ee4eed47b021
andrieljose <andrielscript@gmail.com>
2023-04-04 11:46:03 -0300

    fix problem of image projects not properly showing

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -4,7 +4,9 @@ import { motion } from "framer-motion";
 const container = {
   hidden: {},
   visible: {
-    transition: { staggerChildren: 0.2 },
+    transition: {
+      staggerChildren: 0.2,
+    },
   },
 };
 
@@ -15,14 +17,17 @@ const projectVariant = {
 
 const Project = ({ title }) => {
   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-  bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
   return (
     <motion.div variants={projectVariant} className="relative">
       <div className={overlayStyles}>
         <p className="text-2xl font-playfair">{title}</p>
-        <p className="mt-7">Ut aliquip laboris minim consectetur non.</p>
+        <p className="mt-7">
+          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+          porttitor accumsan tincidunt.
+        </p>
       </div>
       <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
     </motion.div>
@@ -34,7 +39,7 @@ const Projects = () => {
     <section id="projects" className="pt-48 pb-48">
       {/* HEADINGS */}
       <motion.div
-        className="md:w-2/4 mx-auto text-center"
+        className="md:w-2/5 mx-auto text-center"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true, amount: 0.5 }}
@@ -46,15 +51,16 @@ const Projects = () => {
       >
         <div>
           <p className="font-playfair font-semibold text-4xl">
-            <span className="text-orange">PRO</span>JECTS
+            <span className="text-red">PRO</span>JECTS
           </p>
           <div className="flex justify-center mt-5">
-            <LineGradient width="w-1/3" />
+            <LineGradient width="w-2/3" />
           </div>
         </div>
         <p className="mt-10 mb-10">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Nisi hic impedit officia.
+          Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
+          fames odio in at. At magna ornare dictum lectus. Purus massa morbi
+          purus nec eget eleifend ut elit.
         </p>
       </motion.div>
 
@@ -62,18 +68,17 @@ const Projects = () => {
       <div className="flex justify-center">
         <motion.div
           className="sm:grid sm:grid-cols-3"
+          variants={container}
           initial="hidden"
           whileInView="visible"
-          viewport={{ once: true, amount: 0.5 }}
-          transition={{ duration: 0.5 }}
-          variants={container}
+          viewport={{ once: true, amount: 0.2 }}
         >
           {/* ROW 1 */}
           <div
             className="flex justify-center text-center items-center p-10 bg-red
-          max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
           >
-            BEATIFUL USER INTERFACES
+            BEAUTIFUL USER INTERFACES
           </div>
           <Project title="Project 1" />
           <Project title="Project 2" />
@@ -84,12 +89,11 @@ const Projects = () => {
           <Project title="Project 5" />
 
           {/* ROW 3 */}
-
           <Project title="Project 6" />
           <Project title="Project 7" />
           <div
             className="flex justify-center text-center items-center p-10 bg-blue
-          max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
           >
             SMOOTH USER EXPERIENCE
           </div>
