fb8f4f2352ec95c62c9f99ff92d0cc17fce68d3d
andrieljose <andrielscript@gmail.com>
2023-04-04 11:46:40 -0300

    change top bar color

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -20,7 +20,7 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
 const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isDesktop = useMediaQuery("(min-width: 768px)");
-  const navbarBackground = isTopOfPage ? "" : "bg-blue";
+  const navbarBackground = isTopOfPage ? "" : "bg-deep-blue";
 
   return (
     <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
