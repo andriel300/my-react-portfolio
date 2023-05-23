0e8dc3adebec41d61081c88474d822ac0ca1a819
andrieljose <andrielscript@gmail.com>
2023-05-23 20:10:04 -0300

    Update navbar.jsx
    change colors

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
@@ -58,7 +59,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
           </div>
         ) : (
           <button
-            className="rounded-full bg-red p-2"
+            className="rounded-full bg-orange p-2"
             onClick={() => setIsMenuToggled(!isMenuToggled)}
           >
             <img alt="menu-icon" src="assets/menu-icon.svg" />
@@ -67,11 +68,11 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
 
         {/* MOBILE MENU POPUP */}
         {!isDesktop && isMenuToggled && (
-          <div className="fixed right-0 bottom-0 h-full bg-gradient-rainblue w-[300px]">
+          <div className="fixed right-0 bottom-0 h-full bg-dark-grey w-[300px]">
             {/* CLOSE ICON */}
             <div className="flex justify-end p-12">
               <button
-                className="rounded-full bg-red p-2"
+                className="rounded-full bg-orange p-2"
                 onClick={() => setIsMenuToggled(!isMenuToggled)}
               >
                 <img alt="close-icon" src="../assets/close-icon.svg" />
