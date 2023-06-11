c92414f441b1ef0ae7c497872fa062764bc14344
andrieljose <andrielscript@gmail.com>
2023-06-11 16:48:38 -0300

    add react-icons library and remove local icons png, update hover effect

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,53 +1,77 @@
-const SocialMediaIcons = () => {
+import {
+  BsLinkedin,
+  BsTwitter,
+  BsFacebook,
+  BsInstagram,
+  BsGithub,
+} from "react-icons/bs";
+import { motion } from "framer-motion";
+
+const SocialMediaIcons = ({ isLanding }) => {
+  const socialMediaIconColor = isLanding ? "text-black-text" : "text-white";
+  const socialMediaIconDarkColor = isLanding ? "text-white" : "text-black-text";
+
   return (
-    <div className="flex justify-center md:justify-start my-10 gap-7">
-      <a
-        className="hover:opacity-50 transition duration-500"
+    <div
+      className={`flex justify-center md:justify-start gap-4 ${socialMediaIconColor} dark:${socialMediaIconDarkColor}`}
+    >
+      <SocialMediaIconLink
         href="https://www.linkedin.com/andriel-j"
         target="_blank"
         rel="noreferrer"
+        isLanding={isLanding}
       >
-        <img alt="linkedin-link" src="../assets/linkedin.png" />
-      </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
+        <BsLinkedin size={30} />
+      </SocialMediaIconLink>
+      <SocialMediaIconLink
         href="https://www.twitter.com/andriel_j"
         target="_blank"
         rel="noreferrer"
+        isLanding={isLanding}
       >
-        <img alt="twitter-link" src="../assets/twitter.png" />
-      </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
+        <BsTwitter size={30} />
+      </SocialMediaIconLink>
+      <SocialMediaIconLink
         href="https://www.facebook.com/andriel300"
         target="_blank"
         rel="noreferrer"
+        isLanding={isLanding}
       >
-        <img alt="facebook-link" src="../assets/facebook.png" />
-      </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
+        <BsFacebook size={30} />
+      </SocialMediaIconLink>
+      <SocialMediaIconLink
         href="https://www.instagram.com/andriel.j30"
         target="_blank"
         rel="noreferrer"
+        isLanding={isLanding}
       >
-        <img alt="instagram-link" src="../assets/instagram.png" />
-      </a>
-      <a
-        className="hover:opacity-50 transition duration-500"
+        <BsInstagram size={30} />
+      </SocialMediaIconLink>
+      <SocialMediaIconLink
         href="https://www.github.com/andriel300"
         target="_blank"
         rel="noreferrer"
+        isLanding={isLanding}
       >
-        <img
-          className="bg-white rounded-full"
-          width="30px"
-          height="30px"
-          alt="github-link"
-          src="../assets/github.png"
-        />
-      </a>
+        <BsGithub size={30} />
+      </SocialMediaIconLink>
     </div>
+  );
+};
+
+const SocialMediaIconLink = ({ href, children, isLanding }) => {
+  const socialMediaIconColor = isLanding ? "text-black-text" : "text-white";
+  const socialMediaIconDarkColor = isLanding ? "text-white" : "text-black-text";
+  return (
+    <motion.a
+      href={href}
+      className={`hover:opacity-80 transition duration-500 ${socialMediaIconColor} dark:${socialMediaIconDarkColor}`}
+      target="_blank"
+      rel="noopener noreferrer"
+      whileHover={{ scale: 1.2 }}
+    >
+      {children}
+    </motion.a>
   );
 };
 
