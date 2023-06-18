6c29d3898568c4a534365b39f346f1f83fe7881a
andrieljose <andrielscript@gmail.com>
2023-06-18 14:28:40 -0300

    add dark theme

--- src/components/LineGradient.jsx
+++ src/components/LineGradient.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/prop-types */
 const LineGradient = ({ width = "w-full" }) => {
-  return <div className={`h-0.5 ${width} bg-gradient-rainblue`} />;
+  return (
+    <div className={`h-0.5 ${width} dark:bg-gradient-rainblue bg-black`} />
+  );
 };
 
 export default LineGradient;
